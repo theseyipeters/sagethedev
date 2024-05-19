4b99d2c6c7bc6f17d64d4f3bad48ad7c8b9a0f00
devseyipeters <seyi@usecrafter.com>
2024-05-19 08:00:04 +0100

    more ui fixes

--- src/components/InputField.js
+++ src/components/InputField.js
@@ -20,7 +20,7 @@ export default function InputField({
 				id={id}
 				name={name}
 				placeholder={placeholder}
-				className="placeholder:text-blue/20 dark:placeholder:text-white/20 text-blue dark:text-white placeholder:text-sm w-full bg-transparent rounded-md py-[10px] px-[14px] border-0.5 border-blue dark:border-white focus:outline-none text-sm md:text-base lg:text-base"
+				className="placeholder:font-grotta placeholder:text-blue/20 dark:placeholder:text-white/20 text-blue dark:text-white placeholder:text-sm w-full bg-transparent rounded-md py-[10px] px-[14px] border-0.5 border-blue dark:border-white focus:outline-none text-sm md:text-base lg:text-base"
 				value={value}
 				onChange={onChange}
 				required={isRequired}
